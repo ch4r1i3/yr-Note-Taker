42432c3393e0ac1c6ecf56ff867b8370b71a4949
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-02 21:21:19 -0500

    server finished

--- server.js
+++ server.js
@@ -9,19 +9,12 @@ const allNotes = require('./Develop/db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
